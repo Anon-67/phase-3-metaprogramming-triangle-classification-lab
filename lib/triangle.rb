b8b0a363e1a9f9cf2ffc43ffb78033bb47b93582
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-03-16 09:53:04 -0500

    commit tree 46f93736a06240acc048800a77167eaad8686810

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2  
+    @length3 = length3
+  end
+
+  def kind
+    self.valid?
+    if @length1 == @length2 && @length2 == @length3
+      :equilateral
+    elsif @length1 == @length2 || @length2 == @length3 || @length3 == @length1
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def valid?
+    if @length1 <= 0 || @length2 <= 0 || @length3 <= 0
+      raise TriangleError
+    elsif @length1 + @length2 <= @length3 || @length2 + @length3 <= @length1 || @length3 + @length1 <= @length2
+      raise TriangleError
+    else
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "Not a valid triangle"
+  end
+  
 end
